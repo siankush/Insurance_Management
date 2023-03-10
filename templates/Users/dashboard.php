063d141a8f4def945af92d3a968a8dbafa3d21df
TEQ <abishekkumar1611@gmail.com>
2023-03-10 15:38:47 +0530

    change

--- templates/Users/dashboard.php
+++ templates/Users/dashboard.php
@@ -16,7 +16,8 @@
                  <div class="justify-content-end d-flex">
                   <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
                     <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                     <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
+                     <i class="mdi mdi-calendar"></i> Today (1252 Jan 2021)
+                     <!-- Check -->
                     </button>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
                       <a class="dropdown-item" href="#">January - March</a>
